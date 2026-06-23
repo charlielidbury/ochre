701a5b327fa7277b940f7506bfdb9b45901570e9
Charlie Lidbury <charlie.lidbury@icloud.com>
2026-06-23 13:24:04 +0200

    Get quicksort compiling with verus

--- competitors/verus/src/main.rs
+++ competitors/verus/src/main.rs
@@ -1,28 +1,50 @@
+use vstd::prelude::*;
+
+verus! {
+
 /// arranges arr into
 /// [<p, ...] ++ [p] ++ [>=p, ...]
-fn partition(arr: &mut [i32]) -> usize {
+fn partition(arr: &mut [i32]) -> (pivot: usize)
+    requires arr.len() >= 1
+    ensures
+        0 <= pivot < final(arr).len(),
+        final(arr).len() == old(arr).len()
+{
     let p = arr[arr.len() - 1];
 
     let mut lo = 0; // index of first element NOT in low range
-    for hi in 0..arr.len() - 1 {
+    let mut hi = 0;
+    while hi < arr.len() - 1
+        invariant
+            lo <= hi < arr.len(),
+            arr.len() == old(arr).len()
+        decreases arr.len() - hi,
+    {
         if arr[hi] < p {
             // hi is in the wrong place
             // move it into the low region
-
-            arr.swap(hi, lo);
+            let tmp = arr[hi]; //arr.swap(hi, lo);
+            arr[hi] = arr[lo];
+            arr[lo] = tmp;
 
             lo += 1;
         }
+        hi += 1;
     }
 
-    // arrr = [<p, ...] ++ [>=p, ...] ++ [p]
-    arr.swap(arr.len() - 1, lo);
-    // arrr = [<p, ...] ++ [p] ++ [>=p, ...]
+    // arr = [<p, ...] ++ [>=p, ...] ++ [p]
+    let pivot = arr[arr.len() - 1]; //arr.swap(arr.len() - 1, lo);
+    arr[arr.len() - 1] = arr[lo];
+    arr[lo] = pivot;
+    // arr = [<p, ...] ++ [p] ++ [>=p, ...]
 
     lo
 }
 
-fn quicksort(arr: &mut [i32]) {
+
+fn quicksort(arr: &mut [i32])
+    decreases arr.len()
+{
     if arr.len() <= 1 {
         return;
     }
@@ -35,6 +57,8 @@ fn quicksort(arr: &mut [i32]) {
     // Sort each half
     quicksort(lo);
     quicksort(hi);
+}
+
 }
 
 fn main() {
